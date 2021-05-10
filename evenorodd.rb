8f6cde44b85b8b48e6e54e876319c93c35e16eab
Chindala-Pavan <64826287+Chindala-Pavan@users.noreply.github.com>
2021-05-10 11:26:16 +0530

    Update evenorodd.rb

--- evenorodd.rb
+++ evenorodd.rb
@@ -5,10 +5,9 @@ loop do
     n=gets.chomp.to_i
     if n%2==0
         print "#{n} even number"
-    
-    print "do you want to continue(y/n)?"
-    t=gets.chomp.to_sym
-    break if t==:N || t==:n || t==:no || t==:NO
+        print "do you want to continue(y/n)?"
+        t=gets.chomp.to_sym
+        break if t==:N || t==:n || t==:no || t==:NO
     else
         print "#{n} odd number"
         break
